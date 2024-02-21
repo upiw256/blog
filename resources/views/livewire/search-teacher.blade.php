116bce62838b022fa350fcd57d7334e9206c4ce0
upiw256 <bilqimlb@gmail.com>
2024-02-21 15:54:04 +0700

    rensponsif table teacher

--- resources/views/livewire/search-teacher.blade.php
+++ resources/views/livewire/search-teacher.blade.php
@@ -1,28 +1,30 @@
 <div>
     <div>
         <input type="text" wire:model.live="search" class="form-control" placeholder="Cari nama...">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">nama</th>
-                    <th scope="col">mapel</th>
-                    <th scope="col">tempat lahir</th>
-                    <th scope="col">tanggal lahir</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($teachers as $teacher)
-                <tr>
-                    <td>{{ $teacher->id }}</td>
-                    <td>{{ $teacher->nama }}</td>
-                    <td>{{ $teacher->bidang_studi_terakhir }}</td>
-                    <td>{{ $teacher->tempat_lahir }}</td>
-                    <td>{{ $teacher->tanggal_lahir }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="table-responsive mt-3">
+            <table class="table table-striped table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">nama</th>
+                        <th scope="col">mapel</th>
+                        <th scope="col">tempat lahir</th>
+                        <th scope="col">tanggal lahir</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($teachers as $teacher)
+                    <tr>
+                        <td>{{ $teacher->id }}</td>
+                        <td>{{ $teacher->nama }}</td>
+                        <td>{{ $teacher->bidang_studi_terakhir }}</td>
+                        <td>{{ $teacher->tempat_lahir }}</td>
+                        <td>{{ $teacher->tanggal_lahir }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
         {{ $teachers->links() }}
     </div>
     
