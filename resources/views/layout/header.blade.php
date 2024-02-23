fba1e7f37ac7cdf3867592d9f2efe1f47b598896
upiw256 <bilqimlb@gmail.com>
2024-02-23 09:11:56 +0700

    fix header dropdown menu

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -133,31 +133,18 @@
             <nav id="navbar" class="navbar order-last order-lg-0">
                 <ul>
                     <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-                    <li><a class="nav-link scrollto" href="#about">About</a></li>
                     <li><a class="nav-link scrollto" href="#news">News</a></li>
-                    <li><a class="nav-link scrollto" href="#visimisi">Visi & Misi</a></li>
-                    <li><a class="nav-link scrollto" href="#ekstra">Ekstra kulikuler</a></li>
-                    <li><a class="nav-link scrollto" href="#guru">Data Guru</a></li>
-                    <li><a class="nav-link scrollto " href="#achievement">Achievement</a></li>
-                    <li><a class="nav-link scrollto " href="#kepsek">Profile Kepsek</a></li>
-                    <li><a class="nav-link scrollto" href="#team">Team</a></li>
-                    <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-                        <ul>
-                            <li><a href="#">Drop Down 1</a></li>
-                            <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                                <ul>
-                                    <li><a href="#">Deep Drop Down 1</a></li>
-                                    <li><a href="#">Deep Drop Down 2</a></li>
-                                    <li><a href="#">Deep Drop Down 3</a></li>
-                                    <li><a href="#">Deep Drop Down 4</a></li>
-                                    <li><a href="#">Deep Drop Down 5</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Drop Down 2</a></li>
-                            <li><a href="#">Drop Down 3</a></li>
-                            <li><a href="#">Drop Down 4</a></li>
-                        </ul>
-                    </li> -->
+                    <li class="dropdown"><a href="#"><span>Informasi Sekolah</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a class="nav-link scrollto" href="#visimisi">Visi & Misi</a></li>
+                        <li><a class="nav-link scrollto" href="#ekstra">Ekstra kulikuler</a></li>
+                        <li><a class="nav-link scrollto" href="#guru">Data Guru</a></li>
+                        <li><a class="nav-link scrollto " href="#achievement">Achievement</a></li>
+                        <li><a class="nav-link scrollto " href="#kepsek">Profile Kepsek</a></li>
+                        <li><a class="nav-link scrollto" href="#team">Team Management</a></li>
+                    </ul>
+                </li>
+                <li><a class="nav-link scrollto" href="#about">About</a></li>
                     <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
