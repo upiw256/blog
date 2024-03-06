d264e0cac2e3e2e1bc8c8f13f5e82f3b694ce1d4
upiw256 <bilqimlb@gmail.com>
2024-03-06 20:02:57 +0700

    staf fix

--- app/Filament/Resources/StafResource.php
+++ app/Filament/Resources/StafResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\StafResource\Pages;
 use App\Filament\Resources\StafResource\RelationManagers;
-use App\Models\Staf;
+use App\Models\staf;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
