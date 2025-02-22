1320589f906460b2b1e09cebae41a5d9ee942378
upiw256 <bilqimlb@gmail.com>
2025-02-22 23:51:49 +0700

    setting vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    server: {
+        https: true,
+        // atau jika Anda ingin menggunakan sertifikat custom
+        // https: {
+        //   key: fs.readFileSync('/path/to/your/server.key'),
+        //   cert: fs.readFileSync('/path/to/your/server.crt'),
+        // },
+    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
